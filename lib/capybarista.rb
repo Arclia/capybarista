08f93831e6747990ed47538a6edcb6b02e84ce58
Brian Lauber <blauber@jibe.com>
2013-07-03 00:06:51 -0400

    Renamed Capybarista::Capybarista to Capybarista::Session.  This class now behaves as a proxy to Capybara::Session

--- lib/capybarista.rb
+++ lib/capybarista.rb
@@ -1,21 +1,35 @@
 
 module Capybarista
 
-  class Capybarista
+  class Session
 
-    attr_reader :session
+    attr_reader :basis
 
     def initialize(capybara_session)
-      @session = capybara_session
+      @basis = capybara_session
+    end
+
+    def self.for(input)
+      if input.is_a? Capybarista::Session
+        return input
+      else
+        return Capybarista::Session.new input
+      end
     end
 
 
     # Returns the list of inputs that require some form
     # of iteraction w/ the user.
-    def all_fields
-      fields = @session.all(:xpath, "//*[self::input | self::textarea | self::select][not(./@type = 'submit' or ./@type = 'image' or ./@type = 'hidden' or ./@type='button')]")
+    def all_fields(options = {})
+      fields = @basis.all(:xpath, "//*[self::input | self::textarea | self::select][not(./@type = 'submit' or ./@type = 'image' or ./@type = 'hidden' or ./@type='button')]", options)
+    end
+
+
+    def method_missing(name, *args, &block)
+      @basis.public_send(name, *args, &block)
     end
 
   end
+
 
 end
